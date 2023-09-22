4d8775fa3a2fc7f5e00f157a31308e7642e0aab0
Layne <Layne@atguigu.com>
2023-09-22 09:59:25 +0800

    gitee push

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,25 +2,9 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git222");
-        System.out.println("hello git333");
-        System.out.println("hello hot-fox");
-        System.out.println("hello hot-fox");
-        System.out.println("hello hot-fox");
-        System.out.println("1111111");
 
-        System.out.println("测试合并冲突----master 提交本地库");
-        System.out.println("测试合并冲突-----hot-fox提交本地库");
-
+        System.out.println("码云ttestest");
         System.out.println("push");
-        
-        System.out.println("pull");
-
-
-        System.out.println("码云test");
-        System.out.println("push");
-
 
 
     }
